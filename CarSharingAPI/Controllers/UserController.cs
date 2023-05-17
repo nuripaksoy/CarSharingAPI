d08f08ad1c98a2baa977ede8ac42e1cf3f55f398
nuripaksoyy <nuri.paksoy@yf.com.tr>
2023-05-17 19:52:29 +0300

    Add UserController - UserDTO

--- CarSharingAPI/Controllers/UserController.cs
+++ CarSharingAPI/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using CarSharingAPI.Data;
+using CarSharingAPI.Migrations;
 using CarSharingAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -17,6 +18,14 @@ namespace CarSharingAPI.Controllers
         }
 
         [HttpGet]
+        public async Task<ActionResult<List<User>>> GetUserList()
+        {
+            var users = await _context.Users
+                .ToListAsync();
+            return users;
+        }
+
+        [HttpGet]
         [Route("{id:int}")]
         public async Task<IActionResult> GetUser([FromRoute] int id)
         {
@@ -26,6 +35,53 @@ namespace CarSharingAPI.Controllers
                 return NotFound();
             }
             return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(UserDTO userDTO)
+        {
+            var user = new User()
+            {
+                UserName = userDTO.UserName,
+                FullName = userDTO.FullName,
+                Password = userDTO.Password,
+                PhoneNumber = userDTO.PhoneNumber
+            };
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return Ok(user);
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<IActionResult> UpdateUser([FromRoute] int id, UserDTO userDTO)
+        {
+            var user = _context.Users.Find(id);
+            if (user != null)
+            {
+                user.UserName = userDTO.UserName;
+                user.Password = userDTO.Password;
+                user.FullName = userDTO.FullName;
+                user.PhoneNumber = userDTO.PhoneNumber;
+
+                await _context.SaveChangesAsync();
+                return Ok(user);
+            }
+            return NotFound();
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<IActionResult> DeleteUser([FromRoute] int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+                return Ok(user);
+            }
+            return NotFound();
         }
     }
 }
---
+++ CarSharingAPI/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CarSharingAPI
+{
+    public class UserDTO
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
